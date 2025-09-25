4f8289eaa432648a990b98c15df699ded1349eef
Sir. Oscar MG <sir.oscargatheru@gmail.com>
2025-09-25 13:34:09 +0300

    Delete solutions/problem4 directory

--- solutions/problem4/problem4.py
+++
@@ -1,8 +0,0 @@
-def is_palindrome(n):
-    s = str(n)
-    return s == s[::-1]
-
-# Example usage:
-if __name__ == "__main__":
-    num = int(input("Enter a number: "))
-    print(is_palindrome(num))
