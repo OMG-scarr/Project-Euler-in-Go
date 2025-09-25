e87283fbd7f9b3b0bb8c31fd787a724f066969a0
Sir. Oscar MG <sir.oscargatheru@gmail.com>
2025-09-25 13:10:59 +0000

    Update Problem5.go

--- solutions/problem5.go
+++ solutions/problem5.go
@@ -1,8 +1,50 @@
 //LCM (Least Common Multiple)
 
-package main 
+package main
 
-func main(){ 
-  
-fmt.Println("This is the solution to problem 5")
+import "fmt"
+
+// Returns a slice of prime factors for n
+func prime_factors(n int) []int {
+	var factors []int
+	for i := 2; i <= n; i++ {
+		for n%i == 0 {
+			factors = append(factors, i)
+			n /= i
+		}
+	}
+	return factors
+}
+
+// Helper: counts the occurrences of each prime in a slice
+func count_factors(factors []int) map[int]int {
+	counts := make(map[int]int)
+	for _, f := range factors {
+		counts[f]++
+	}
+	return counts
+}
+
+// Computes the LCM from the prime factors of all numbers 2..n
+func lcm_from_factors(n int) int {
+	max_counts := make(map[int]int)
+	for i := 2; i <= n; i++ {
+		counts := count_factors(prime_factors(i))
+		for prime, count := range counts {
+			if count > max_counts[prime] {
+				max_counts[prime] = count
+			}
+		}
+	}
+	lcm := 1
+	for prime, count := range max_counts {
+		for i := 0; i < count; i++ {
+			lcm *= prime
+		}
+	}
+	return lcm
+}
+
+func main() {
+	fmt.Println(lcm_from_factors(20))
 }
