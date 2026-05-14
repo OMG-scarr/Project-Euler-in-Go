840edaa13d67993b083dbed621b36a82c2394f6c
Sir. Oscar MG <sir.oscargatheru@gmail.com>
2026-05-14 21:40:05 +0300

    Solved Problem 7

---
+++ solutions/problem7.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func nthPrimeNumber(x int) int {
+	primes := []int{}
+	n := 2
+
+	for len(primes) < x {
+		isPrime := true
+
+		for i := 2; i <= n/2; i++ {
+			if n%i == 0 {
+				isPrime = false
+				break
+			}
+		}
+		if isPrime {
+			primes = append(primes, n)
+		}
+		n++
+	}
+
+	return primes[x-1]
+}
+
+func main() {
+	fmt.Println(nthPrimeNumber(10))
+}
